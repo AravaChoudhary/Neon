c592dca2b3e7d460a8c5de4e9ddbd5d4d307d087
AravaChoudhary <aravachoudhary11@gmail.com>
2023-11-01 21:17:04 +0530

    new files added

--- Controllers/UserControllers.js
+++ Controllers/UserControllers.js
@@ -1,4 +1,5 @@
 const UserSchema = require('../Schemas/Users')
+const bcrypt = require('bcrypt')
 
 exports.getForm = (req,res)=>{
     res.send(
@@ -21,31 +22,52 @@ exports.getForm = (req,res)=>{
 exports.showResults = (req,res)=>{
     const {name,email,mobile,password,location} = req.body
 
-UserSchema.insertMany({ name:name , email:email , mobile:mobile , password:password ,location:location}).then((result)=>{
-    console.log(result)
-    if(result.lenght>0)
-    {
-        res.status(200).send({status:200,meaasge : "User Registered Succesfully"})
-    }
-    else
-    {
-        res.status(400).send({status:400,meaage: "User Registratio Failed || Please Try Again"})
-    }
-}).catch((err)=>{
-    console.log(err.name)
-    console.log(err.code)
-    console.log(err.message)
-    if(err.code == 11000)
-    {
-        res.status(400).send({status : 400,message : `User already exist with this ${err.message.split('{')[1].split(":")[0]} : ${err.message.split('{')[1].split(":")[1].replaceAll(`"`," ").replace('}',' ')} `})
-    }
-    else if(err.name == 'ValidationError')
-    {
-        res.status(400).send({status : 400 , message : `${err.message.split(':')[1].trim().toUpperCase()} is required for registration`})
-    }
-    else
-    {
-        res.ststus(500).send({status : 500, message: "Something Went Wrong"})
-    }
-})
+     bcrypt.genSalt(10, function(err,salt){
+         if(err)
+         {
+             res.status(500).send({status:500,message : "Something Went Wrong" })
+         }
+         else
+         {
+         bcrypt.hash(password,salt, function(err,hash){
+             if(err)
+             {
+                 res.status(500).send({status:500,message : "Something Went Wrong" })
+             }
+             else
+             {
+                UserSchema.insertMany({ name:name , email:email , mobile:mobile , password:hash ,location:location}).then((result)=>{
+                    console.log(result)
+                    if(result.lenght>0)
+                    {
+                        res.status(400).send({status:400,meaage: "User Registratio Failed || Please Try Again"})
+                    }
+                    else
+                    {
+                        res.status(200).send({status:200,meaasge : "User Registered Succesfully"})
+                    }
+                }).catch((err)=>{
+                    console.log(err.name)
+                    console.log(err.code)
+                    console.log(err.message)
+                    if(err.code == 11000)
+                    {
+                        res.status(400).send({status : 400,message : `User already exist with this ${err.message.split('{')[1].split(":")[0]} : ${err.message.split('{')[1].split(":")[1].replaceAll(`"`," ").replace('}',' ')} `})
+                    }
+                    else if(err.name == 'ValidationError')
+                    {
+                        res.status(400).send({status : 400 , message : `${err.message.split(':')[1].trim().toUpperCase()} is required for registration`})
+                    }
+                    else
+                    {
+                        res.ststus(500).send({status : 500, message: "Something Went Wrong"})
+                    }
+                })
+             }
+         })
+         }
+
+     })
+
+ 
 }
